b3e4cde387021881f5e2baa82be8198bbe9a39c8
Enn <ennqndrei@gmail.com>
2026-04-15 17:12:57 +0300

    update doc

--- Dev/App/main.c
+++ Dev/App/main.c
@@ -3,14 +3,6 @@
 signed main(void) {
         DEBUG_TRACE();
         core_init();
-        
-        window_create(&(WindowSpecification) {
-                .width = 1280,
-                .height = 720,
-                .name = "Digital Logic Simulation",
-                .has_msaa = true,
-                .is_resizable = true
-        });
 
         core_run();
         core_term();
